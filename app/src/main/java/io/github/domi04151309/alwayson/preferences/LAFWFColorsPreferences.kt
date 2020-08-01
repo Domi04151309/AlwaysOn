0c0eccc6f4e04d2eae82d51f55bff6aa85bc0707
Domi04151309 <domi04151309business@gmail.com>
2020-08-01 11:42:17 +0200

    Add color preferences

--- app/src/main/java/io/github/domi04151309/alwayson/preferences/LAFWFColorsPreferences.kt
+++ app/src/main/java/io/github/domi04151309/alwayson/preferences/LAFWFColorsPreferences.kt
@@ -1,15 +1,13 @@
 package io.github.domi04151309.alwayson.preferences
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
 import io.github.domi04151309.alwayson.R
 import io.github.domi04151309.alwayson.objects.Theme
 
-class LAFWatchFacePreferences : AppCompatActivity(),
+class LAFWFColorsPreferences : AppCompatActivity(),
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,25 +35,7 @@ class LAFWatchFacePreferences : AppCompatActivity(),
 
     class PreferenceFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            addPreferencesFromResource(R.xml.pref_laf_watch_face)
-            findPreference<Preference>("ao_style")?.setOnPreferenceClickListener {
-                startActivity(Intent(context, AlwaysOnLookActivity::class.java))
-                true
-            }
-            findPreference<Preference>("ao_colors")?.setOnPreferenceClickListener {
-                startActivity(Intent(context, LAFWFColorsPreferences::class.java))
-                true
-            }
-            findPreference<Preference>("pref_filter_notifications")?.setOnPreferenceClickListener {
-                startActivity(Intent(context, FilterNotificationsActivity::class.java))
-                true
-            }
-            val prefAodScale = findPreference<SeekBarPreference>("pref_aod_scale") ?: return
-            prefAodScale.summary = resources.getString(R.string.pref_look_and_feel_display_size_summary, prefAodScale.value + 50)
-            prefAodScale.setOnPreferenceChangeListener { preference, newValue ->
-                preference.summary = resources.getString(R.string.pref_look_and_feel_display_size_summary, (newValue as Int) + 50)
-                true
-            }
+            addPreferencesFromResource(R.xml.pref_laf_wf_colors)
         }
     }
 }
