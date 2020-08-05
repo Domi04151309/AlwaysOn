f73d83b28fef2f6189358c53ff2347eb4ab66e92
Domi04151309 <domi04151309business@gmail.com>
2020-08-05 13:19:49 +0200

    Improve media controls

--- app/src/main/java/io/github/domi04151309/alwayson/alwayson/AlwaysOnOnActiveSessionsChangedListener.kt
+++ app/src/main/java/io/github/domi04151309/alwayson/alwayson/AlwaysOnOnActiveSessionsChangedListener.kt
@@ -42,11 +42,16 @@ class AlwaysOnOnActiveSessionsChangedListener(
     internal fun updateMediaState() {
         if (controller != null) {
             viewHolder.musicLayout.visibility = View.VISIBLE
-            viewHolder.musicTxt.text = resources.getString(
-                    R.string.music,
-                    controller?.metadata?.getString(MediaMetadata.METADATA_KEY_ARTIST),
-                    controller?.metadata?.getString(MediaMetadata.METADATA_KEY_TITLE)
-            )
+            val artist = controller?.metadata?.getString(MediaMetadata.METADATA_KEY_ARTIST) ?: ""
+            val title = controller?.metadata?.getString(MediaMetadata.METADATA_KEY_TITLE) ?: ""
+            if (artist == "")
+                viewHolder.musicTxt.text = title
+            else if (title == "")
+                viewHolder.musicTxt.text = artist
+            else if (artist.length > 20 || title.length > 20)
+                viewHolder.musicTxt.text = resources.getString(R.string.music_long, artist, title)
+            else
+                viewHolder.musicTxt.text = resources.getString(R.string.music, artist, title)
         } else {
             viewHolder.musicLayout.visibility = View.GONE
         }
