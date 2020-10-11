46b555a7b2f3f367bdec3663ec02db6316179d70
Domi04151309 <domi04151309business@gmail.com>
2020-10-11 12:14:22 +0200

    Try adding new intent filter

--- app/src/main/java/io/github/domi04151309/alwayson/receivers/AutoStart.kt
+++ app/src/main/java/io/github/domi04151309/alwayson/receivers/AutoStart.kt
@@ -9,7 +9,7 @@ import io.github.domi04151309.alwayson.services.ForegroundService
 class AutoStart : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if(intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
             ContextCompat.startForegroundService(context, Intent(context, ForegroundService::class.java))
         }
     }
