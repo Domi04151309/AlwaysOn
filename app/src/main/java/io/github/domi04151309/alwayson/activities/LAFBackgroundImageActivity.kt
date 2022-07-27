9bc77ef0ebbd21e812b6ce841226e5b43cc70fa5
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-07-27 12:14:31 +0200

    Add custom background option
    Closes #57

--- app/src/main/java/io/github/domi04151309/alwayson/activities/LAFBackgroundImageActivity.kt
+++ app/src/main/java/io/github/domi04151309/alwayson/activities/LAFBackgroundImageActivity.kt
@@ -1,10 +1,21 @@
 package io.github.domi04151309.alwayson.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +23,10 @@ import io.github.domi04151309.alwayson.R
 import io.github.domi04151309.alwayson.adapters.LayoutListAdapter
 import io.github.domi04151309.alwayson.helpers.P
 import io.github.domi04151309.alwayson.helpers.Theme
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.lang.Integer.min
+
 
 class LAFBackgroundImageActivity : AppCompatActivity() {
 
@@ -33,7 +48,8 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
         R.drawable.unsplash_filip_baotic_1,
         R.drawable.unsplash_tyler_lastovich_1,
         R.drawable.unsplash_tyler_lastovich_2,
-        R.drawable.unsplash_tyler_lastovich_3
+        R.drawable.unsplash_tyler_lastovich_3,
+        R.drawable.ic_more
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +61,45 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
         preview = findViewById(R.id.preview)
         layoutList = findViewById(R.id.layout_list)
 
+        val customImageResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    if (result.data == null) return@registerForActivityResult
+                    Thread {
+                        val inputStream: InputStream? = contentResolver.openInputStream(
+                            result.data?.data
+                                ?: return@Thread
+                        )
+
+                        var bitmap = BitmapFactory.decodeStream(inputStream) ?: return@Thread
+                        val size = min(bitmap.width, bitmap.height)
+                        bitmap = Bitmap.createBitmap(
+                            bitmap,
+                            (bitmap.width - size) / 2,
+                            (bitmap.height - size) / 2,
+                            size,
+                            size
+                        )
+                        if (bitmap.width > 1080) {
+                            bitmap = Bitmap.createScaledBitmap(
+                                bitmap,
+                                1080,
+                                1080,
+                                true
+                            )
+                        }
+
+                        val os = ByteArrayOutputStream()
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 60, os)
+                        val encoded: String = Base64.encodeToString(
+                            os.toByteArray(), Base64.DEFAULT
+                        )
+                        runOnUiThread { preview.setImageBitmap(bitmap) }
+                        prefs.edit().putString(P.CUSTOM_BACKGROUND, encoded).apply()
+                    }.start()
+                }
+            }
+
         layoutList.layoutManager = LinearLayoutManager(this).apply {
             orientation = LinearLayoutManager.HORIZONTAL
         }
@@ -54,7 +109,31 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
             resources.getStringArray(R.array.pref_ao_background_image_array_display),
             object : LayoutListAdapter.OnItemClickListener {
                 override fun onItemClick(view: View, position: Int) {
-                    preview.setImageResource(drawables[position])
+                    if (position == ITEM_CUSTOM) {
+                        showCustomImage()
+                        if (!hasPermission()) {
+                            ActivityCompat.requestPermissions(
+                                this@LAFBackgroundImageActivity,
+                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                                0
+                            )
+                        }
+                        if (hasPermission()) {
+                            customImageResult.launch(Intent(Intent.ACTION_PICK).apply {
+                                setDataAndType(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    "image/*"
+                                )
+                            })
+                        } else {
+                            Toast.makeText(
+                                this@LAFBackgroundImageActivity,
+                                R.string.missing_permissions,
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+
+                    } else preview.setImageResource(drawables[position])
                     value = when (position) {
                         ITEM_NONE -> P.BACKGROUND_IMAGE_NONE
                         ITEM_DANIEL_OLAH_1 -> P.BACKGROUND_IMAGE_DANIEL_OLAH_1
@@ -69,6 +148,7 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
                         ITEM_TYLER_LASTOVICH_1 -> P.BACKGROUND_IMAGE_TYLER_LASTOVICH_1
                         ITEM_TYLER_LASTOVICH_2 -> P.BACKGROUND_IMAGE_TYLER_LASTOVICH_2
                         ITEM_TYLER_LASTOVICH_3 -> P.BACKGROUND_IMAGE_TYLER_LASTOVICH_3
+                        ITEM_CUSTOM -> P.BACKGROUND_IMAGE_CUSTOM
                         else -> P.BACKGROUND_IMAGE_DEFAULT
                     }
                 }
@@ -96,6 +176,7 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
                 P.BACKGROUND_IMAGE_TYLER_LASTOVICH_1 -> ITEM_TYLER_LASTOVICH_1
                 P.BACKGROUND_IMAGE_TYLER_LASTOVICH_2 -> ITEM_TYLER_LASTOVICH_2
                 P.BACKGROUND_IMAGE_TYLER_LASTOVICH_3 -> ITEM_TYLER_LASTOVICH_3
+                P.BACKGROUND_IMAGE_CUSTOM -> ITEM_CUSTOM
                 else -> ITEM_NONE
             }
         )
@@ -106,9 +187,21 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
         prefs.edit().putString(P.BACKGROUND_IMAGE, value).apply()
     }
 
+    internal fun showCustomImage() {
+        val decoded = Base64.decode(prefs.getString(P.CUSTOM_BACKGROUND, ""), 0)
+        preview.setImageBitmap(BitmapFactory.decodeByteArray(decoded, 0, decoded.size))
+    }
+
     private fun setSelectedItem(adapter: LayoutListAdapter, position: Int) {
-        preview.setImageResource(drawables[position])
+        if (position == ITEM_CUSTOM) showCustomImage()
+        else preview.setImageResource(drawables[position])
         adapter.setSelectedItem(position)
+    }
+
+    internal fun hasPermission(): Boolean {
+        return applicationContext.checkSelfPermission(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
@@ -125,5 +218,6 @@ class LAFBackgroundImageActivity : AppCompatActivity() {
         private const val ITEM_TYLER_LASTOVICH_1 = 10
         private const val ITEM_TYLER_LASTOVICH_2 = 11
         private const val ITEM_TYLER_LASTOVICH_3 = 12
+        private const val ITEM_CUSTOM = 13
     }
 }
