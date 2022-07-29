5cbee2ec27bd7d3be914822f77e72d3592941c28
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2022-07-29 17:18:09 +0200

    Add dialog before device admin activation

--- app/src/main/java/io/github/domi04151309/alwayson/activities/PermissionsActivity.kt
+++ app/src/main/java/io/github/domi04151309/alwayson/activities/PermissionsActivity.kt
@@ -1,10 +1,13 @@
 package io.github.domi04151309.alwayson.activities
 
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.view.Gravity
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -13,6 +16,7 @@ import io.github.domi04151309.alwayson.R
 import io.github.domi04151309.alwayson.helpers.Root
 import io.github.domi04151309.alwayson.helpers.Theme
 
+
 class PermissionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +24,9 @@ class PermissionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, PreferencePermissions())
-                .commit()
+            .beginTransaction()
+            .replace(R.id.settings, PreferencePermissions())
+            .commit()
     }
 
     class PreferencePermissions : PreferenceFragmentCompat() {
@@ -32,9 +36,36 @@ class PermissionsActivity : AppCompatActivity() {
                 startActivity(Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS))
                 true
             }
+            findPreference<Preference>("device_admin")?.setOnPreferenceClickListener {
+                val dialogView = layoutInflater.inflate(R.layout.dialog_device_admin, null, false)
+                val editText = dialogView.findViewById<EditText>(R.id.editText)
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.device_admin)
+                    .setView(dialogView)
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        if (editText.text.toString().trim() == "19") {
+                            startActivity(Intent().apply {
+                                component = ComponentName(
+                                    "com.android.settings",
+                                    "com.android.settings.Settings\$DeviceAdminSettingsActivity"
+                                )
+                            })
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                R.string.dialog_device_admin_error,
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ -> }
+                    .show()
+                true
+            }
             findPreference<Preference>("root_mode")?.setOnPreferenceClickListener {
                 if (!Root.request()) {
-                    val toast = Toast.makeText(context, R.string.setup_root_failed, Toast.LENGTH_LONG)
+                    val toast =
+                        Toast.makeText(context, R.string.setup_root_failed, Toast.LENGTH_LONG)
                     toast.setGravity(Gravity.CENTER, 0, 0)
                     toast.show()
                     (it as SwitchPreference).isChecked = false
