2fedcb1d95cb3eca9140123a79e9ce4644c69960
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-01-06 22:35:58 +0100

    Change java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,11 +45,11 @@ android {
         buildConfig = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     lint {
         disable += "MissingTranslation"
